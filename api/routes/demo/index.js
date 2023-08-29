34168efd6c3bc2e37fff072c0daafb415d2ee151
Fabio De Paz <fabio.depaz@telusinternational.com>
2023-08-29 14:49:09 -0600

    feat: adding looped simulation

--- api/routes/demo/index.js
+++ api/routes/demo/index.js
@@ -1,28 +1,31 @@
-const { error } = require("console");
-
 
 const simulateFailure = (ms=3000) => new Promise(r => setTimeout(r, ms));
 
+const handleRequest = async (id)=> {
+  switch(id){
+    case '1000': 
+      await simulateFailure()
+      break
+    case '2000':
+      throw new Error('Some error...')
+    case '9000':
+        let index = 0
+        while(index < parseInt(id)*100){
+          console.log(`${index} looping... `)
+          index++
+        }
+    default:
+        const message = `Returning data for id: '${id}'`
+        return { status: "ok", message}
+  }
+}
 
 module.exports = async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
     const {query: {id}} = request
-    const message = `Returning data for id: '${id}'`
+    
+    response = await handleRequest(id)
 
-    if(id == 1000) {
-      await simulateFailure()
-    }
-
-    switch(id){
-      case '1000': 
-        await simulateFailure()
-        break
-      case '2000':
-        throw new Error('Some error...')
-    }
-
-
-    reply.code(200)
-    reply.send({ status: "ok", message})
+    reply.send(response)
   })
 }
